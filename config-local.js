827c402fa9bbd6e144bd7ee5dd8c08768699d2c5
Kavya <2k22cse077@kiot.ac.in>
2026-02-26 19:45:26 +0530

    Updated

---
+++ config-local.js
@@ -0,0 +1,9 @@
+// config-local.js (your local file - NOT on GitHub)
+window.HRM_CONFIG = {
+    emailjs: {
+        enabled: true,
+        publicKey: 'egn0RAP5LGiXtfSjR',
+        serviceId: 'service_l9cu4wu',
+        // ... real config
+    }
+};
---
+++ config.js
@@ -0,0 +1,9 @@
+window.HRM_CONFIG = {
+  emailjs: {
+    enabled: false,
+    publicKey: "",
+    serviceId: "",
+    leaveTemplate: "",
+    meetingTemplate: ""
+  }
+};
